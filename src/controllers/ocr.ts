6fbc09efe08a5b2461a437464b83c892b3bb0ee6
sebasyii <syii.xh@gmail.com>
2022-09-27 10:42:44 +0800

    Added dpi with new params

--- src/controllers/ocr.ts
+++ src/controllers/ocr.ts
@@ -10,44 +10,45 @@ export const ocrSchema: Schema = {
     isBase64: true,
     exists: true,
     errorMessage: 'Invalid base64 string',
-  }
-}
+  },
+};
 
 export const ocrController = async (req: Request, res: Response, next: NextFunction) => {
-
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new TypeError(errors.array()[0].msg))
+    return next(new TypeError(errors.array()[0].msg));
   }
 
   const inputBuffer = req.body.pdf;
 
-  const pdf:Buffer = Buffer.from(inputBuffer, 'base64');
+  const pdf: Buffer = Buffer.from(inputBuffer, 'base64');
 
   const options = {
     density: 100,
-    saveFilename: "untitled",
-    savePath: "./images",
-    format: "png",
+    saveFilename: 'untitled',
+    savePath: './images',
+    format: 'png',
     width: 600,
-    height: 600
+    height: 600,
   };
 
-  let result:ToBase64Response[]
+  let result: ToBase64Response[];
   try {
     result = await fromBuffer(pdf, options).bulk(-1, true);
+  } catch (e) {
+    return next(e);
   }
-  catch (e) { return next(e) };
 
   let resp: { [text: string]: string[] } = { text: [] };
 
   for (const page of result) {
     try {
-      const imgBuffer:Buffer = Buffer.from(page.base64, 'base64');
-      const text:string = await tesseract.recognize(imgBuffer, { lang: "eng", oem: 1, psm: 3 });
+      const imgBuffer: Buffer = Buffer.from(page.base64, 'base64');
+      const text: string = await tesseract.recognize(imgBuffer, { lang: 'eng', oem: 2, psm: 5, dpi: 300 });
       resp.text.push(text);
+    } catch (e) {
+      return next(e);
     }
-    catch (e) { return next(e) };
   }
 
   res.status(200).send(resp);
